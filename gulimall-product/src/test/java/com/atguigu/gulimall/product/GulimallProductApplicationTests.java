7d124d779d56b5db1bcd8e43730ea84d641d2335
HaveDate <734873995@qq.com>
2021-01-25 12:22:45 +0800

    推送了日常代码

--- gulimall-product/src/test/java/com/atguigu/gulimall/product/GulimallProductApplicationTests.java
+++ gulimall-product/src/test/java/com/atguigu/gulimall/product/GulimallProductApplicationTests.java
@@ -1,38 +1,60 @@
 package com.atguigu.gulimall.product;
 
-import com.atguigu.gulimall.product.entity.BrandEntity;
-import com.atguigu.gulimall.product.service.BrandService;
-import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.aliyun.oss.OSS;
+import com.aliyun.oss.OSSClient;
+import com.aliyun.oss.OSSClientBuilder;
+import com.atguigu.gulimall.product.controller.CategoryController;
+import com.atguigu.gulimall.product.service.CategoryService;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-import java.util.List;
+import javax.annotation.Resource;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.Arrays;
 
+@Slf4j
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class GulimallProductApplicationTests {
+public class GulimallProductApplicationTests {
+
+
+//    @Resource
+//    OSS ossClient;
 
     @Autowired
-    BrandService brandService;
+    CategoryService categoryService;
 
     @Test
-    public void contextLoads() {
-
-//        BrandEntity brandEntity = new BrandEntity();
-//        brandEntity.setBrandId(1L);
-//        brandEntity.setName("小米");
-//
-//        brandEntity.setName("华为");
-//        brandService.save(brandEntity);
-//        System.out.println("保存成功....");
-
-//        brandService.updateById(brandEntity);
-
-        List<BrandEntity> list = brandService.list(new QueryWrapper<BrandEntity>().eq("brand_id", 1l));
-        list.forEach((item)->{
-            System.out.println(item);
-        });
-
+    public void testFindPath(){
+        Long[] catelogPath = categoryService.findCatelogPath(225L);
+        log.info("完整路径:{}", Arrays.asList(catelogPath));
     }
+
+//    @Test
+//    public void testUpload() throws FileNotFoundException {
+////        // Endpoint以杭州为例，其它Region请按实际情况填写。
+////        String endpoint ="oss-cn-guangzhou.aliyuncs.com";
+////    // 云账号AccessKey有所有API访问权限，建议遵循阿里云安全最佳实践，创建并使用RAM子账号进行API访问或日常运维，请登录 https://ram.console.aliyun.com 创建。
+////        String accessKeyId = "LTAI4GE95wXZop5FjozfxFSv";
+////        String accessKeySecret = "CbXtd0ebYVulxMr5N6qtqq6sUy0Lq9";
+////
+////// 创建OSSClient实例。
+////        OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
+//
+//// 上传文件流。
+//        InputStream inputStream = new FileInputStream("C:\\Users\\Administrator\\Documents\\Tencent Files\\734873995\\FileRecv\\MobileFile\\Screenshot_2020-10-14-21-44-37-671_com.tencent.mo.jpg");
+//        ossClient.putObject("gulimall-zouxuan", "Screenshot_2020-10-14-21-44-37-671_com.tencent.mo.jpg", inputStream);
+//
+//// 关闭OSSClient。
+//        ossClient.shutdown();
+//
+//        System.out.println("上传成功！！！");
+//    }
 
 }
--- gulimall-third-party/src/main/java/com/atguigu/gulimall/thirdparty/GulimallThirdPartyApplication.java
+++ gulimall-third-party/src/main/java/com/atguigu/gulimall/thirdparty/GulimallThirdPartyApplication.java
@@ -1,17 +1,16 @@
-package com.atguigu.gulimall.product;
+package com.atguigu.gulimall.thirdparty;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @EnableDiscoveryClient
-@MapperScan("com.atguigu.gulimall.product.dao")
 @SpringBootApplication
-public class GulimallProductApplication {
+public class GulimallThirdPartyApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(GulimallProductApplication.class, args);
+        SpringApplication.run(GulimallThirdPartyApplication.class, args);
     }
 
 }
